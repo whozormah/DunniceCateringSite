5bbf7002050950f759a72edf0e6c41417cb2a292
uzoma <uzoma.udoma@gmail.com>
2023-01-13 15:26:54 +0100

    form

--- main.js
+++ main.js
@@ -4,5 +4,14 @@ const faq = document.querySelectorAll('.faqs');
 faq.forEach(faq => {
     faq.addEventListener('click', () =>{
         faq.classList.toggle('open');
+
+        //change plus Icon
+        const icon = faq.querySelector('.faq-icon i');
+        if(icon.className === 'uil uil-angle-right-b'){
+            icon.className = 'uil uil-angle-down';
+        }
+        else{
+            icon.className = 'uil uil-angle-right-b';
+        }
     })
 })
